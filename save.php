81f338bc48ea8af21611d24cc391033a35fa1cda
Dániel <nagydaniel04@yahoo.com>
2016-05-19 11:20:05 +0300

    save.php validation name and email

--- save.php
+++ save.php
@@ -20,6 +20,15 @@
         $email=$_POST["email"];
         $country_id=$_POST["country"];
         $county_id=$_POST["county"];
+        //validation
+        $isname="/^[A-ZÁÉÚŰŐÓÜÖÍ][a-zéáűőúöüóí]+ [A-ZÁÉÚŰŐÓÜÖÍ][a-zéáűőúöüóí]+$/";
+        if (empty($name)||!preg_match($isname,$name)||strlen($name)>30 ) {
+            echo 'Incorrect name';
+        }
+        if(empty($email)||strlen($email)>30||(filter_var($email, FILTER_VALIDATE_EMAIL) === false)){
+            echo 'Incorrect email';
+        }
+                //!preg_match_all("^[A-Za-z0-9]+@[a-z]+\.[a-z]$",$email, $passw))
         $sql="INSERT INTO person(name, email, country_id,county_id)
             VALUES('$name','$email','$country_id','$county_id')";
         if(mysqli_query($conn,$sql)){
